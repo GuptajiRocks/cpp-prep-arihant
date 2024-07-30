2bc91742d01a3f0133f843c0e393916efd0b055f
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-07-30 10:43:10 +0530

    first commit

---
+++ 30jul/one.cpp
@@ -0,0 +1,11 @@
+//
+// Created by ariha on 30-07-2024.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World" << endl;
+    return 0;
+}
---
+++ 30jul/one.h
@@ -0,0 +1,16 @@
+//
+// Created by ariha on 30-07-2024.
+//
+
+#ifndef ONE_H
+#define ONE_H
+
+
+
+class one {
+
+};
+
+
+
+#endif //ONE_H
