2961e94c860bc08d4d7c540f0f9e222100835aed
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-07-30 11:59:50 +0530

    class creation and object in cpp

--- 30jul/two.cpp
+++ 30jul/two.cpp
@@ -2,10 +2,39 @@
 // Created by ariha on 30-07-2024.
 //
 
+// only writing comments because naya naya shauk hai
 #include <iostream>
 using namespace std;
 
+class Dog {
+    // We have defined the data members as public so that they will be accessible
+public:
+    string breed;
+    int age;
+    string color;
+    string size;
+
+    // printname function defined the printing of the data that will be stored in this
+    void printname() {
+        cout << "Breed is :" << breed << endl;
+        cout << "Age is: " << age << endl;
+        cout << "Color is: " << color << endl;
+        cout << "Size is: " << size << endl;
+    }
+};
+// main function begins
 int main() {
-    cout << "My name is" << endl;
+    // object of Dog class has been created
+    Dog dog;
+    // feeding in the data in appropriate variables
+    dog.breed = "Street dog";
+    dog.age = 100;
+    dog.color = "Black";
+    dog.size = "Big ass dog";
+
+    // now the data has been fed, we will call the function we created to print every data
+
+    dog.printname();
+    // sherr, now program has been executed.
     return 0;
 }
