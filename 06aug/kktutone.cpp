c62a494f0a9ff260a35a9f3bc1d51b5f1ae9c3b6
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-08-06 10:34:38 +0530

    06 aug tut

---
+++ 06aug/kktutone.cpp
@@ -0,0 +1,19 @@
+//
+// Created by ariha on 06-08-2024.
+//
+
+#include <iostream>
+using namespace std;
+
+void juicy() {
+    int a[10];
+    a[size(a)-10] = 50;
+    cout << a;
+
+}
+
+int main() {
+    juicy();
+
+}
+
