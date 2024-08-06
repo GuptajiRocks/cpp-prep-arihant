a600f61c26f4cb72e0e8fe628dcfbb63f1260e64
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-08-06 22:57:35 +0530

    armstrong number

--- 06aug/three.cpp
+++ 06aug/three.cpp
@@ -3,20 +3,48 @@
 //
 
 #include <iostream>
+#include <math.h>
 using namespace std;
 
-void s1() {
-    string sher[5] = {"Arihant","Vishnu","Arastu","Vansh","Sarthak"};
-    string* vor;
-    vor = sher;
+// void s1() {
+//     string sher[5] = {"Arihant","Vishnu","Arastu","Vansh","Sarthak"};
+//     string* vor;
+//     vor = sher;
+//
+//     for (int i=0; i<size(sher); i++) {
+//         cout << sher[i] << endl;
+//     }
+//
+// }
+//
+// void s2() {
+//     int var[] = {1,2,3,4,4,5,6};
+//     cout << size(var);
+// }
 
-    for (int i=0; i<size(sher); i++) {
-        cout << sher[i] << endl;
+int armstrong(string x) {
+    int j = stoi(x);
+    int ep = size(x);
+    double res = 0;
+    while (j != 0) {
+        res = res + (pow((j%10),ep));
+        j = j/10;
     }
-
+    if (res == stoi(x)) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
 int main() {
-    s1();
+    string inp;
+    cin >> inp;
+    int res = armstrong(inp);
+    if (res == 1) {
+        cout << "Armstrong" << endl;
+    } else {
+        cout << "Not Armstrong" << endl;
+    }
 }
 
