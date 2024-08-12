1aa617ca4d71a857500fffd383b514095c78ba2e
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-08-12 10:13:47 +0530

    row major order concepts

--- 09aug/six.cpp
+++ 09aug/six.cpp
@@ -2,4 +2,11 @@
 // Created by ariha on 09-08-2024.
 //
 
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int main() {
+    long long a;
+
+}
---
+++ 12aug/one.cpp
@@ -0,0 +1,28 @@
+//
+// Created by ariha on 12-08-2024.
+//
+
+#include <iostream>
+using namespace std;
+
+/*
+ *This was column major order
+ *Address (A[i][j] = B + W* ((J-LC)*M) * (I-LR)
+ *
+ *This is row major order
+ * Address (A[i][j] = B + W * (I-LR)*N  + ((J-LC))
+ */
+
+// Now major A[8][6] = 100 + 1 * (
+/* A[8][6] = 100 + 1 * (7)*10 * (6-1)
+ * = 100 + 70*5
+ * = 450
+ */
+
+void row_major_order() {
+
+
+}
+int main() {
+
+}
