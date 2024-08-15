5a563ac264e685838ae91620306693212f431539
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-08-15 17:37:36 +0530

    trying something

---
+++ 13aug/one.cpp
@@ -0,0 +1,31 @@
+//
+// Created by ariha on 13-08-2024.
+//
+
+#include <iostream>
+
+#include <vector>
+using namespace std;
+
+
+// Given any two positive integers N and K, Find the minimum number of digits that can be removed from the number N such that after removal
+// the number is divisible by 10K. Print -1 if this is impossible.
+
+/* Explanation:
+ * if N = 10203027 and K = 2, then we if remove 3,2,7 then number become 10200 which is divisible by 102
+ */
+// Why
+
+void rengo() {
+ string n;
+ cin >> n;
+ for (int i = 0; i<n.length(); i++) {
+  
+
+
+ }
+
+}
+
+
+
---
+++ 15aug/one.cpp
@@ -0,0 +1,10 @@
+//
+// Created by ariha on 15-08-2024.
+//
+#include <iostream>
+using namespace std;
+
+void happy() {
+    cout << "";
+    
+}
