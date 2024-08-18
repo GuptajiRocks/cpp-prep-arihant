3a41a6fc8bcdb8869823ae047c5b55eb5c6e7797
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-08-18 10:42:11 +0530

    trying happy number

---
+++ 18aug/two.cpp
@@ -0,0 +1,25 @@
+//
+// Created by ariha on 18-08-2024.
+//
+
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+// Trying to build the program for happy number
+
+using namespace std;
+
+int recalc() {
+
+
+
+}
+int main() {
+    string temp;
+    cin >> temp;
+    vector<vector<int>> res;
+
+
+}
+
