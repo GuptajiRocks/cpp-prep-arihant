83b3e32bf18e83cfecc95cd3bf1173d2d5cac40b
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-08-20 11:18:51 +0530

    20 aug start

--- 19aug/one.cpp
+++ 19aug/one.cpp
@@ -3,12 +3,13 @@
 //
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 // Program for bubble sort
 
-void sorting(int arr[]) {
-    int len = sizeof(arr)/sizeof(arr[0]);
+void sortings(vector<int> arr) {
+    int len = arr.size();
     bool swapp;
 
     for (int i = 0; i<len-1; i++) {
@@ -19,15 +20,20 @@ void sorting(int arr[]) {
                 swap(arr[j], arr[j+1]);
                 swapp = true;
             }
-
         }
-
         if (!swapp) {
             break;
         }
     }
+
+    for (int num : arr) {
+        cout << num << " ";
+    }
 }
 
 int main() {
+    vector<int> arr = {17,19,22,1,0,45};
+
+    sortings(arr);
 
 }
---
+++ 20aug/one.cpp
@@ -0,0 +1,6 @@
+//
+// Created by ariha on 20-08-2024.
+//
+
+// Function prototyping
+
