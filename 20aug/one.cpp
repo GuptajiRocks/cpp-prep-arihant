cb176cf50dffb3ed98edf7f1a803ba7ca22e1f17
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-08-20 12:04:17 +0530

    call by ref and add

--- 20aug/one.cpp
+++ 20aug/one.cpp
@@ -6,7 +6,26 @@
 
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
+void sitt(int &a, int &b) {
+    cout << a+b;
+}
 
+void call_byadd(int* lauda, int* lauda1) {
+    cout << (*lauda)-(*lauda1);
+}
+
+int main() {
+    // Return by address and return by reference
+    int a,b;
+    cin >> a >> b;
+
+    // sitt(a,b);
+
+    call_byadd(&a, &b);
+
+
+
+
+}
