4684391c41116789f5b9c01054dd60e758a9f261
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-08-22 16:42:59 +0530

    basic array explain

---
+++ 22aug/two.cpp
@@ -0,0 +1,28 @@
+//
+// Created by ariha on 22-08-2024.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int len;
+    cin >> len;
+    float lb1[len];
+
+    for (int i = 0; i < len; i++) {
+        cin >> lb1[i];
+    }
+
+    for (int i = 0; i < len; i++) {
+        cout << lb1[i];
+    }
+
+
+
+
+
+
+
+
+}
