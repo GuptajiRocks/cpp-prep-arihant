8db649441254deeb64673c0e571135c33d8e6267
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-08-26 14:27:51 +0530

    4 coursera courses done, yeah baby

---
+++ 26aug/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by ariha on 26-08-2024.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    cout << "Greetings from Illinois!" << endl;
+}
