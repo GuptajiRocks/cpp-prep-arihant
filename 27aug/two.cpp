69ee88ddbe1aba0e6a572105846fba8249dee037
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-08-27 23:43:37 +0530

    leetcode sort color done

---
+++ 27aug/two.cpp
@@ -0,0 +1,12 @@
+//
+// Created by ariha on 27-08-2024.
+//
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void sortc(vector<int>& nums) {
+    for (int i = 0; i < nums.size(); i++) {
+        cout << nums[i];
+    }
+}
