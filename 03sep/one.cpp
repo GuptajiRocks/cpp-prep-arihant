9962fd5d63d0c191e1053c9bf1880fc0ef85b699
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-09-04 09:53:28 +0530

    rotate array done, and linked list trying

---
+++ 03sep/one.cpp
@@ -0,0 +1,26 @@
+//
+// Created by ariha on 03-09-2024.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+
+};
+int main() {
+    int arr[] = {1,2,3,4,5};
+    int r = 2;
+    int temp = arr[5];
+    for (int i = 5; i >=0; i--) {
+        arr[i] = arr[i+1];
+
+
+
+
+    }
+
+}
+
