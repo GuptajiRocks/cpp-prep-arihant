80d18dce78e5e2ef2cc2bec6f1fcbc1c6807a70c
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-09-06 22:50:39 +0530

    need to install clion once again

--- 06sep/two.cpp
+++ 06sep/two.cpp
@@ -4,3 +4,33 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+
+class Pat {
+    public:
+    int id;
+    string name;
+    int age;
+    string disease;
+};
+
+void ins(vector<Pat>& arr, int n) {
+
+}
+
+int main() {
+    // int n;
+    // vector<Pat> arr;
+    // Pat temp;
+    // cin >> temp.id;
+    // cin >> temp.name;
+    // cin >> temp.age;
+    // cin >> temp.disease;
+
+    // arr.push_back(temp);
+    // Pat temp;
+
+    // string ch1;
+
+    cout << false;    
+
+}
