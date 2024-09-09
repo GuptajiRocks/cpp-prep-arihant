5b26d9bf6e2cf66d887d6ee6a2433a232c732cda
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-09-09 10:00:39 +0530

    stack concept new type shit

---
+++ 09sep/two.cpp
@@ -0,0 +1,19 @@
+//
+// Created by ariha on 09-09-2024.
+//
+
+#include <iostream>
+using namespace std;
+
+// Stack principle
+
+int main() {
+    // LIFO principle
+    // Insertion deletion both can be performed from both top and bottom
+    // Overflow condition if stack is already full
+    /*
+     * Topics to learn
+     * 1. Stack code structure
+     * 2. Underflow if empty, overflow if stack is full.
+     * 3.
+}
