0f0ea733c17ba2b7840d7778bbb751a19523f7fd
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-09-17 16:03:20 +0530

    mutiple data member LL

---
+++ 17sep/one.cpp
@@ -0,0 +1,30 @@
+//
+// Created by ariha on 17-09-2024.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int coeff;
+    int expo;
+    Node* next;
+
+    Node() {
+        coeff = 0;
+        expo = 0;
+        next = nullptr;
+    }
+
+    Node(int c, int e) {
+        coeff = c;
+        expo = e;
+        next = nullptr;
+    }
+};
+
+int main() {
+
+
+}
