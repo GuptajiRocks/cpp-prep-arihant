c34092d107700353c79cb9ae16e113674f13632d
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-09-20 01:14:38 +0530

    CF 2010A try

---
+++ 20sep/one.cpp
@@ -0,0 +1,26 @@
+//
+// Created by ariha on 20-09-2024.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int len;
+    cin >> len;
+
+    int arr[len];
+    for (int i = 0; i < len; i++) {
+        cin >> arr[i];
+    }
+
+    int sum = 0;
+    for (int i = 0; i < len; i++) {
+        if (i%2 != 0) {
+            sum = sum - arr[i];
+        } else {
+            sum = sum + arr[i];
+        }
+    }
+
+    cout << sum << endl;
+}
