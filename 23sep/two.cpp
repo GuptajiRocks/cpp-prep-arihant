984862c7f928fc060697140b7a3e4e803253a287
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-09-23 09:20:20 +0530

    LL revise

--- 23sep/two.cpp
+++ 23sep/two.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 
 class Node {
+public:
     int data;
     Node* next;
 
@@ -16,6 +17,27 @@ class Node {
 };
 
 int main() {
+    cout << "Enter first element: ";
+    int tc;
+    cin >> tc;
+
+    Node* head = new Node(tc);
+    Node* temp = head;
+
+    for (int i = 0; i < 5; i++) {
+        cin >> tc;
+        temp->next = new Node(tc);
+        temp = temp->next;
+    }
+
+    Node* trav = head;
+
+    while (trav != nullptr) {
+        cout << trav->data << " ";
+        trav = trav->next;
+    }
+
+    cout << endl;
 
 
 }
