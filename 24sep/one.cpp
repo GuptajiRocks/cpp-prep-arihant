b6efd696174a806dcf8cf62e8afdf70050c686b7
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-09-24 10:08:46 +0530

    working tree addition

---
+++ 24sep/one.cpp
@@ -0,0 +1,26 @@
+//
+// Created by ariha on 24-09-2024.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Jesus {
+    int num;
+    int val;
+};
+
+int main() {
+    vector<Jesus> arr;
+
+    int len;
+    cin >> len;
+
+    int tarr[len];
+    for (int i = 0; i < len; i++) {
+        cin >> tarr[i];
+    }
+
+    
+
+}
