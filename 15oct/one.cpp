af6adaf64cbf3c337148681164f3e7c8199bad74
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-10-15 10:00:27 +0530

    polynomial handling

--- 15oct/one.cpp
+++ 15oct/one.cpp
@@ -61,14 +61,50 @@ public:
     }
 };
 
-int main() {
-    Polynomial s1;
-    s1.addHead(10);
-    s1.addElem(12);
+// int main() {
+//     Polynomial s1;
+//     s1.addHead(10);
+//     s1.addElem(12);
+//
+//     s1.display();
+//
+//     return 0;
+// }
 
-    s1.display();
+
+class Pol {
+public:
+    int coeff;
+    int expo;
+    Pol* next;
+
+    Pol(int c, int e) {
+        coeff = c;
+        expo = e;
+        next = nullptr;
+    }
+};
+
+int main() {
+    cout << "Polynomial 1: " << endl;
+    int len;
+    cout << "Enter highest power: "; cin >> len;
+    int tc, te;
+    int i = 0;
+    cout << "Enter " << (i+1) << " coeff and expo of the polynomial: ";
+    cin >> tc >> te;
+    Pol* head1 = new Pol(tc, te);
+    Pol* temp1 = head1;
+    for (int j = i+1; j < len; j++) {
+        cin >> tc >> te;
+        temp1->next = new Pol(tc, te);
+        temp1 = temp1->next;
+    }
+
+    cout << "Polyomial 1 added";
 
     return 0;
+
 }
 
 
