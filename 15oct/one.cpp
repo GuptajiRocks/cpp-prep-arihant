29827837d0754e1fd261d5e89a7cbcc9daaf493b
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-10-15 09:39:50 +0530

    postfix eval 1

--- 15oct/one.cpp
+++ 15oct/one.cpp
@@ -1,3 +1,36 @@
 //
 // Created by ariha on 15-10-2024.
 //
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+    int data;
+    Node* next;
+
+
+};
+
+void postfixeval() {
+    // 6 2 3 * / 3 4 * + 3 2 * -
+    // Input Stack
+    /* 6    6
+     * 2    6 2
+     * 3    6 2 3
+     * *    6 6
+     * /    1
+     * 3    1 3
+     * 4    1 3 4
+     * *    1 12
+     * +    13
+     * 3    13 3
+     * 2    13 3 2
+     * *    13 6
+     * -    7
+     *
+     * Final answer evaluation is 7.
+     */
+}
+
+
+
