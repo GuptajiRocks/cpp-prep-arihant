959af364923d1ef9c8144e3f739370e19889cf9a
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-11-06 13:02:56 +0530

    heap array implementation

---
+++ 06nov/one.cpp
@@ -0,0 +1,35 @@
+//
+// Created by ariha on 06-11-2024.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void heapify(int arr[], int N, int i) {
+    int largest = i;
+    int l = 2*i + 1;
+    int r = 2*i + 2;
+
+    if (l < N && arr[l] > arr[largest]) {
+        largest = l;
+    }
+
+    if (r < N && arr[r] > arr[largest]) {
+        largest = r;
+    }
+
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+    }
+}
+
+void buildHeap(int arr[], int n) {
+    int idx = (n+1)/2;
+    for (int i = idx; i < n; i++) {
+        heapify(arr, n, idx);
+    }
+}
+
+int main() {
+    int arr[] = {1,2,3,4,5,6};
+}
