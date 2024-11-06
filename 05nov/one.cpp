cca8b04fda86f7cc651414c22d89d2e7b6d3e216
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-11-06 10:53:39 +0530

    added HTML here

---
+++ 05nov/one.cpp
@@ -0,0 +1,25 @@
+//
+// Created by ariha on 05-11-2024.
+//
+
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int c) {
+        data = c;
+        left = right = nullptr;
+    }
+};
+
+class BinarySearchTree {
+    
+};
+int main() {
+
+}
+
