1cc6e2dabe5aadf1fef6063296d56e0537ed6369
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-11-07 12:46:37 +0530

    min heap not working

--- 06nov/one.cpp
+++ 06nov/one.cpp
@@ -10,11 +10,11 @@ void heapify(int arr[], int N, int i) {
     int l = 2*i + 1;
     int r = 2*i + 2;
 
-    if (l < N && arr[l] > arr[largest]) {
+    if (l < N && arr[l] < arr[largest]) {
         largest = l;
     }
 
-    if (r < N && arr[r] > arr[largest]) {
+    if (r < N && arr[r] < arr[largest]) {
         largest = r;
     }
 
@@ -24,12 +24,18 @@ void heapify(int arr[], int N, int i) {
 }
 
 void buildHeap(int arr[], int n) {
-    int idx = (n+1)/2;
-    for (int i = idx; i < n; i++) {
+    int idx = (n-1)/2;
+    for (int i = idx; i >= 0; i--) {
         heapify(arr, n, idx);
     }
 }
 
 int main() {
-    int arr[] = {1,2,3,4,5,6};
+    int arr[] = {2, 12, 1, 7, 3, 14};
+    buildHeap(arr, (sizeof(arr)/sizeof(arr[0])));
+    for (int i = 0; i < (sizeof(arr)/sizeof(arr[0])); i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
 }
