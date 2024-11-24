4020b31bc09e262343ec441060486d0cd20bf77c
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-11-25 00:22:56 +0530

    new day new shit

---
+++ 24nov/two.cpp
@@ -0,0 +1,22 @@
+//
+// Created by ariha on 24-11-2024.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+class BinarySearchTree {
+
+
+};
