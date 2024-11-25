e9e90331262f705299ddd3127f5215927545d782
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-11-25 20:31:06 +0530

    heap

--- 24nov/two.cpp
+++ 24nov/two.cpp
@@ -19,4 +19,5 @@ struct TreeNode {
 class BinarySearchTree {
 
 
+
 };
---
+++ 25nov/two.cpp
@@ -0,0 +1,9 @@
+//
+// Created by ariha on 25-11-2024.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// trying to build heap and do shit lol
+
