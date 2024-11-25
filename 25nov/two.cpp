b7c55c723c30097bd5897b60947ff69b74e3a982
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-11-25 23:31:11 +0530

    minHeap using priority queue STL

--- 25nov/two.cpp
+++ 25nov/two.cpp
@@ -7,3 +7,17 @@ using namespace std;
 
 // trying to build heap and do shit lol
 
+int main() {
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    minHeap.push(10);
+    minHeap.push(5);
+    minHeap.push(15);
+    minHeap.push(2);
+
+    while (!minHeap.empty()) {
+        cout << minHeap.top() << " ";
+        minHeap.pop();
+    }
+    cout << endl;
+    return 0;
+}
