3e76733475b286386d76306c0be8d591d1e19ab1
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-12-07 22:29:20 +0530

    LL revise

--- 07dec/one.cpp
+++ 07dec/one.cpp
@@ -5,6 +5,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct Node {
+    int data;
+    Node* next;
+
+    Node() {
+        next = nullptr;
+    }
+
+    Node(int c) {
+        data = c;
+        next = nullptr;
+    }
+};
+
 int main() {
-    
+    cout << "Enter the elements to be added in LL: ";
+    int n;
+    cin >> n;
+
+    // cout << "Enter elements:";
+    Node* head;
+    Node* temp = head;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Enter element " << i+1 << ": ";
+        int tc;
+        cin >> tc;
+        temp->next = new Node(tc);
+        temp = temp->next;
+    }
+
+    Node* trav = head;
+    trav = trav->next;
+
+    while (trav != nullptr) {
+        cout << "Element: " << trav->data << " " << endl;
+        trav = trav->next;
+    }
+
+    return 0;
 }
