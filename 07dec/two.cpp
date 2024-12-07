ca7e67916597b2251806ac85f168ee044fed4e12
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-12-07 23:37:15 +0530

    anything

---
+++ 07dec/two.cpp
@@ -0,0 +1,43 @@
+//
+// Created by ariha on 07-12-2024.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// trying to build a program to delete element from the middle
+
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int c) {
+        data = c;
+        next = nullptr;
+    }
+
+    Node() {
+        next = nullptr;
+    }
+};
+
+// class LinkedList {
+//     Node* head;
+// public:
+//     LinkedList() {
+//         head = nullptr;
+//     }
+//
+//     void insert(int c) {
+//         Node*
+//     }
+//
+//
+// };
+
+void c1() {
+    // trying from array
+    int n;
+    cout << "How many elements do you wish to enter?: ";
+
+}
