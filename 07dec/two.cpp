808352750ae70fd9529aba800d49cfb5c7e16d4f
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2024-12-08 14:44:37 +0530

    array element delete

--- 07dec/two.cpp
+++ 07dec/two.cpp
@@ -39,5 +39,36 @@ void c1() {
     // trying from array
     int n;
     cout << "How many elements do you wish to enter?: ";
+    cin >> n;
 
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int chc;
+    cout << "Enter the element you wish to be deleted: ";
+    cin >> chc;
+    int co = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == chc) {
+            co = i;
+            break;
+        }
+    }
+
+    for (int i = co; i < n; i++) {
+        arr[i] = arr[i+1];
+    }
+
+    cout << "Updated List: ";
+
+    for (int i = 0; i < n-1; i++) {
+        cout << arr[i] << " ";
+    }
+}
+
+int main() {
+    c1();
 }
